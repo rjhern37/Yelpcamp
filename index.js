e04b8aa7b417f12a77b3caba88d4133be93c7a3f
Ray H <rjhern37@neiu.edu>
2021-04-12 11:33:36 -0500

    fixed error message formatting

--- index.js
+++ index.js
@@ -93,8 +93,9 @@ app.all('*', (req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-	const { statusCode = 500, message = 'Something went wrong!' } = err;
-	res.status(statusCode).render('error');
+	const { statusCode = 500 } = err;
+	if (!err.message) err.message = 'Oh No, Something went wrong!';
+	res.status(statusCode).render('error', { err });
 });
 
 app.listen(3000, () => {
