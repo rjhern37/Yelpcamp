f2c7c8fbb6ce20f0fc71fac8cb4197d787986e9c
Ray H <rjhern37@neiu.edu>
2021-04-12 12:43:33 -0500

    Added other required fields to joi schema

--- index.js
+++ index.js
@@ -51,7 +51,10 @@ app.post(
 		const campgroundSchema = Joi.object({
 			campground: Joi.object({
 				title: Joi.string().required(),
-				price: Joi.number().required().min(0)
+				price: Joi.number().required().min(0),
+				image: Joi.string().required(),
+				location: Joi.string().required(),
+				description: Joi.string().required()
 			}).required()
 		});
 		const { error } = campgroundSchema.validate(req.body);
