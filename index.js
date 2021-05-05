9931619e975a0de449da1fd12a0bcfd3eac1bf12
Ray H <rjhern37@neiu.edu>
2021-05-05 12:30:36 -0500

    configured express session

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const ejsMate = require('ejs-mate');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
 const expressError = require('./utils/expressError');
 const methodOverride = require('method-override');
 
@@ -30,6 +31,18 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+const sessionConfig = {
+	secret: 'thisShouldBeABetterSecret',
+	resave: false,
+	saveUninitialized: true,
+	cookie: {
+		httpOnly: true,
+		expires: Date.now + 1000 * 60 * 60 * 24 * 7,
+		maxAge: 1000 * 60 * 60 * 24 * 7
+	}
+};
+app.use(session(sessionConfig));
 
 app.use('/campgrounds', campgrounds);
 app.use('/campgrounds/:id/reviews', reviews);
