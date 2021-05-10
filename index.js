d0e11292ec54b3f16dff77d41b9df1471059e77b
Ray H <rjhern37@neiu.edu>
2021-05-10 13:13:18 -0500

    added user registration formwith validations

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const expressError = require('./utils/expressError');
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./model/user');
 
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const userRoutes = require('./routes/users');
+const campgroundsRoutes = require('./routes/campgrounds');
+const reviewsRoutes = require('./routes/reviews');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
 	useNewUrlParser: true,
@@ -46,6 +50,13 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 //Middleware that passes 'flash' through to each route making it available to each route if needed
 app.use((req, res, next) => {
 	res.locals.success = req.flash('success');
@@ -53,8 +64,9 @@ app.use((req, res, next) => {
 	next();
 });
 
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundsRoutes);
+app.use('/campgrounds/:id/reviews', reviewsRoutes);
 
 app.get('/', (req, res) => {
 	res.render('home');
