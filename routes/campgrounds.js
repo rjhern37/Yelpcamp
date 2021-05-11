a4cea8d5788bb69adcb1a7e4a8790b441f48628b
Ray H <rjhern37@neiu.edu>
2021-05-11 15:36:07 -0500

    added user authorizations to edit and update requests

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -59,10 +59,15 @@ router.get(
 	'/:id/edit',
 	isLoggedIn,
 	catchAsync(async (req, res) => {
-		const campground = await Campground.findById(req.params.id);
+		const { id } = req.params;
+		const campground = await Campground.findById(id);
 		if (!campground) {
 			req.flash('error', 'Cannot find the Campground you are looking for.');
 			return res.redirect('/campgrounds');
+		}
+		if (!campground.author.equals(req.user._id)) {
+			req.flash('error', 'You do not have permission to do that!');
+			return res.redirect(`/campgrounds/${id}`);
 		}
 		res.render('campgrounds/edit', { campground });
 	})
@@ -74,7 +79,12 @@ router.put(
 	validateCampground,
 	catchAsync(async (req, res) => {
 		const { id } = req.params;
-		const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+		const campground = await Campground.findById(id);
+		if (!campground.author.equals(req.user._id)) {
+			req.flash('error', 'You do not have permission to do that!');
+			return res.redirect(`/campgrounds/${id}`);
+		}
+		const camp = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
 		req.flash('success', 'Successfully updated Campground');
 		res.redirect(`/campgrounds/${campground._id}`);
 	})
