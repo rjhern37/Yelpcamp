7b484395d778572dce506289ca6dfc54424ae713
Ray H <rjhern37@neiu.edu>
2021-05-12 14:33:46 -0500

    reverted back to refactored campground routes to fix campground show page when user logs in

--- routes/users.js
+++ routes/users.js
@@ -5,12 +5,17 @@ const User = require('../model/user');
 const catchAsync = require('../utils/catchAsync');
 const users = require('../controllers/users');
 
-router.route('/register').get(users.renderRegister).post(catchAsync(users.register));
+router.get('/register', users.renderRegister);
 
-router
-	.route('/login')
-	.get(users.renderLogin)
-	.post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.loginUser);
+router.post('/register', catchAsync(users.register));
+
+router.get('/login', users.renderLogin);
+
+router.post(
+	'/login',
+	passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }),
+	users.loginUser
+);
 
 router.get('/logout', users.logoutUser);
 
