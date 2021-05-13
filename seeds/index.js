e5ffb68350c75120f8940c32aeb7ff9a152c9696
Ray H <rjhern37@neiu.edu>
2021-05-13 13:37:17 -0500

    Re-seeded to fix images on index page

--- seeds/index.js
+++ seeds/index.js
@@ -26,10 +26,21 @@ const seedDB = async () => {
 			author: '60997600f361e81c9a4d28d9',
 			location: `${cities[random1000].city}, ${cities[random1000].state}`,
 			title: `${sample(descriptors)} ${sample(places)}`,
-			image: 'https://source.unsplash.com/collection/1952444',
 			description:
 				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-			price
+			price,
+			images: [
+				{
+					url:
+						'https://res.cloudinary.com/daftpunk132/image/upload/v1620930513/Yelpcamp/voivzmrfottjdhy1rcch.jpg',
+					filename: 'Yelpcamp/voivzmrfottjdhy1rcch'
+				},
+				{
+					url:
+						'https://res.cloudinary.com/daftpunk132/image/upload/v1620930313/Yelpcamp/ommcdbbrjngnsouzs64z.jpg',
+					filename: 'Yelpcamp/ommcdbbrjngnsouzs64z'
+				}
+			]
 		});
 		await camp.save();
 	}
