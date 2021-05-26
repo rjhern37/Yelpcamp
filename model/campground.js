5cc5631a81b9bfaf89ce95f7be25860d3f3de383
Ray H <rjhern37@neiu.edu>
2021-05-26 11:06:58 -0500

    Added an Image schema to auto size pics uploaded to a width of 200px

--- model/campground.js
+++ model/campground.js
@@ -3,21 +3,26 @@ const Review = require('./review');
 const { schema } = require('./user');
 const Schema = mongoose.Schema;
 
+
+const ImageSchema = new Schema({
+	url: String,
+	filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function(){
+	return this.url.replace('/upload', '/upload/w_200');
+})
+
 const CampgroundSchema = new Schema({
 	title: String,
+	images: [ImageSchema],
+	price: Number,
+	description: String,
+	location: String,
 	author: {
 		type: Schema.Types.ObjectId,
 		ref: 'User'
 	},
-	images: [
-		{
-			url: String,
-			filename: String
-		}
-	],
-	price: Number,
-	description: String,
-	location: String,
 	reviews: [
 		{
 			type: Schema.Types.ObjectId,
