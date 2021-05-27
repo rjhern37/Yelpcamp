fa24d4087070c4871fdcee6b1d3e0c27cf053563
Ray H <rjhern37@neiu.edu>
2021-05-27 12:39:40 -0500

    Added campground data to cluster map, now tweaking to custom fit

--- seeds/index.js
+++ seeds/index.js
@@ -29,7 +29,10 @@ const seedDB = async () => {
 			description:
 				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 			price,
-			geometry: { type: 'Point', coordinates: [ -157.8583333, 21.3069444 ] },
+			geometry: {
+				type: 'Point',
+				coordinates: [ cities[random1000].longitude, cities[random1000].latitude ]
+			},
 			images: [
 				{
 					url:
