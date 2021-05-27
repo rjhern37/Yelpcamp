f06bf3664eb119db9b6e7895dd39ac767e74c002
Ray H <rjhern37@neiu.edu>
2021-05-27 13:48:33 -0500

    Finalized the vitural for the popup marker on the cluster map

--- model/campground.js
+++ model/campground.js
@@ -47,7 +47,11 @@ const CampgroundSchema = new Schema(
 );
 
 CampgroundSchema.virtual('properties.popUpMarkup').get(function() {
-	return 'I AM POP UP TEXT';
+	return `
+		<strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>
+		<p>
+		${this.description.substring(0, 20)}...
+		</p>`;
 });
 
 CampgroundSchema.post('findOneAndDelete', async function(doc) {
