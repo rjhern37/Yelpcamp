7953e2a58126167d9335da21f8f5e61b88eaedcd
Ray H <rjhern37@neiu.edu>
2021-06-01 13:05:02 -0500

    Added mapbox navigational controls to cluster map and show page map

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -6,6 +6,8 @@ var map = new mapboxgl.Map({
 	zoom: 10
 });
 
+map.addControl(new mapboxgl.NavigationControl());
+
 new mapboxgl.Marker()
 	.setLngLat(campground.geometry.coordinates)
 	.setPopup(new mapboxgl.Popup({ offset: 25 }).setHTML(`<h4>${campground.title}</h4><p>${campground.location}</p>`))
