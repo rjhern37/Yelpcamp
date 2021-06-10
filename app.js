94e2e56a267cdbde21a8fc6a9cb497289b46f0de
Ray H <rjhern37@neiu.edu>
2021-06-10 14:33:06 -0500

    Trying to fix mapbox for mobile

--- app.js
+++ app.js
@@ -81,22 +81,23 @@ app.use(flash());
 app.use(helmet());
 
 const scriptSrcUrls = [
-	'https://stackpath.bootstrapcdn.com',
-	'https://api.tiles.mapbox.com',
-	'https://api.mapbox.com',
-	'https://kit.fontawesome.com',
-	'https://cdnjs.cloudflare.com',
+	'https://stackpath.bootstrapcdn.com/',
+	'https://api.tiles.mapbox.com/',
+	'https://api.mapbox.com/',
+	'https://kit.fontawesome.com/',
+	'https://cdnjs.cloudflare.com/',
 	'https://cdn.jsdelivr.net'
 ];
 const styleSrcUrls = [
-	'https://kit-free.fontawesome.com',
-	'https://stackpath.bootstrapcdn.com',
-	'https://api.mapbox.com',
-	'https://api.tiles.mapbox.com',
-	'https://fonts.googleapis.com',
-	'https://use.fontawesome.com'
+	'https://kit-free.fontawesome.com/',
+	'https://stackpath.bootstrapcdn.com/',
+	'https://api.mapbox.com/',
+	'https://api.tiles.mapbox.com/',
+	'https://fonts.googleapis.com/',
+	'https://use.fontawesome.com/',
+	'https://cdn.jsdelivr.net'
 ];
-const connectSrcUrls = [ 'https://api.mapbox.com', 'https://*.tiles.mapbox.com', 'https://events.mapbox.com' ];
+const connectSrcUrls = [ 'https://api.mapbox.com/', 'https://*.tiles.mapbox.com/', 'https://events.mapbox.com/' ];
 const fontSrcUrls = [];
 app.use(
 	helmet.contentSecurityPolicy({
@@ -106,14 +107,13 @@ app.use(
 			scriptSrc: [ "'unsafe-inline'", "'self'", ...scriptSrcUrls ],
 			styleSrc: [ "'self'", "'unsafe-inline'", ...styleSrcUrls ],
 			workerSrc: [ "'self'", 'blob:' ],
-			childSrc: [ 'blob:' ],
 			objectSrc: [],
 			imgSrc: [
 				"'self'",
 				'blob:',
 				'data:',
-				'https://res.cloudinary.com/douqbebwk/', //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
-				'https://images.unsplash.com'
+				`https://res.cloudinary.com/${process.env.CLOUDINARY_CLOUD_NAME}/`, //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+				'https://images.unsplash.com/'
 			],
 			fontSrc: [ "'self'", ...fontSrcUrls ]
 		}
